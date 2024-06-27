7cb80939f2069f226087696cbd7c2003b2f86001
“agiletoolkit” <bob@electroglide.biz>
2024-06-27 20:47:44 +0000

    added python file and tests

---
+++ tests/test_functions.py
@@ -0,0 +1,12 @@
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+import my_fun
+
+def test_add():
+    assert my_fun.add(1,2)==3
+
+def test_mult():
+    assert my_fun.mult(2,3)==6
