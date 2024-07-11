8b3315dca647ac92a8fa6ef5b79bd5c19f44e7bc
“agiletoolkit” <bob@electroglide.biz>
2024-07-11 20:52:47 +0000

    refactored naming

--- tests/test_math_funtions.py
+++ tests/test_math_funtions.py
@@ -3,13 +3,13 @@ import os.path
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-import my_fun
+import math_functions
 
 def test_add():
-    assert my_fun.add(1,2)==3
+    assert math_functions.add(1,2)==3
 
 def test_mult():
-    assert my_fun.mult(2,3)==6
+    assert math_functions.multiply(2,3)==6
     
 def test_mult2():    
-    assert my_fun.mult(3,3)==9
+    assert math_functions.multiply(3,3)==9
